d5bdc2b228fcbf37c9b8d28d7bf8a38ccc942c84
Jeremy Bertrand <jeremy.bertrand@youboox.fr>
2022-02-24 22:04:35 +0000

    feat: add shop model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_153120) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_220405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_153120) do
     t.string "house_name"
     t.date "start_date"
     t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
