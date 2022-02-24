427dd068e716aff3951f802c9712f011fb8d3d3f
Jeremy Bertrand <jeremy.bertrand@youboox.fr>
2022-02-24 22:24:23 +0000

    feat: add OpeningHour model

---
+++ db/migrate/20220224222342_create_opening_hours.rb
@@ -0,0 +1,12 @@
+class CreateOpeningHours < ActiveRecord::Migration[7.0]
+  def change
+    create_table :opening_hours do |t|
+      t.string :day
+      t.time :opening
+      t.time :closing
+      t.belongs_to :shop, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_220405) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_222342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_220405) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "opening_hours", force: :cascade do |t|
+    t.string "day"
+    t.time "opening"
+    t.time "closing"
+    t.bigint "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_opening_hours_on_shop_id"
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "opening_hours", "shops"
 end
