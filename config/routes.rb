43aa0d04aef206f3d8847dbecead56d61cc1bd2d
Jeremy Bertrand <jeremy.bertrand@youboox.fr>
2022-02-25 00:15:34 +0000

    feat: add a route listing shops

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :shops, only: [] do
+  resources :shops, only: :index do
     scope module: :shops do
       resources :opening_hours, only: :index
     end
